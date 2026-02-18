6c071d90faaa00ed1c04d1dfd0b19d360d2f5919
guifroes1984 <guilhermefroes26@gmail.com>
2026-02-17 22:28:22 -0300

    Start na API.

---
+++ src/main/java/com/guifroes1984/moto_controle/MotoControleApplication.java
@@ -0,0 +1,13 @@
+package com.guifroes1984.moto_controle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoControleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoControleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guifroes1984/moto_controle/MotoControleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guifroes1984.moto_controle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoControleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
