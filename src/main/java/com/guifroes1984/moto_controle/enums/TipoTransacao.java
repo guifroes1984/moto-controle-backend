c836d4c399a515f09c564d8f4c61730a24bf9500
guifroes1984 <guilhermefroes26@gmail.com>
2026-02-19 09:50:11 -0300

    Criado o enum e model de Transação.

---
+++ src/main/java/com/guifroes1984/moto_controle/enums/TipoTransacao.java
@@ -0,0 +1,8 @@
+package com.guifroes1984.moto_controle.enums;
+
+public enum TipoTransacao {
+
+	GANHO, 
+	GASTO
+
+}
---
+++ src/main/java/com/guifroes1984/moto_controle/model/Transacao.java
@@ -0,0 +1,114 @@
+package com.guifroes1984.moto_controle.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TBL_TRANSACOES")
+public class Transacao {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private String TipoTransacao;
+
+	@Column(nullable = false)
+	private String categoria;
+
+	@Column(nullable = false)
+	private Double valor;
+
+	@Column(nullable = false)
+	private LocalDateTime data;
+
+	private String descricao;
+
+	@ManyToOne
+	@JoinColumn(name = "usuario_id", nullable = false)
+	private Usuario usuario;
+
+	public Transacao() {
+	}
+
+	public Transacao(Long id, String tipoTransacao, String categoria, Double valor, LocalDateTime data,
+			String descricao, Usuario usuario) {
+		super();
+		this.id = id;
+		TipoTransacao = tipoTransacao;
+		this.categoria = categoria;
+		this.valor = valor;
+		this.data = data;
+		this.descricao = descricao;
+		this.usuario = usuario;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipoTransacao() {
+		return TipoTransacao;
+	}
+
+	public void setTipoTransacao(String tipoTransacao) {
+		TipoTransacao = tipoTransacao;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+}
